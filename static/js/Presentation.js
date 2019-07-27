7972fcea5445471708fdb34f1649ecc9f25c1b51
jwaliaga <jwaliaga@hotmail.com>
2019-07-27 13:34:31 -0500

    ARIMA Updated

--- static/js/Presentation.js
+++ static/js/Presentation.js
@@ -4,8 +4,8 @@ var myState = {
     zoom: 1
 }
 
-pdfjsLib.getDocument('NatGasPrice.pdf').then((pdf) => {
- 
+pdfjsLib.getDocument(pdf_url).then((pdf) => {
+
     // more code here
     myState.pdf = pdf;
     render();
@@ -14,7 +14,7 @@ pdfjsLib.getDocument('NatGasPrice.pdf').then((pdf) => {
 
 function render() {
     myState.pdf.getPage(myState.currentPage).then((page) => {
- 
+
         // more code here
         var canvas = document.getElementById("pdf_renderer");
         var ctx = canvas.getContext('2d');
@@ -23,10 +23,94 @@ function render() {
 
         canvas.width = viewport.width;
         canvas.height = viewport.height;
-        
+
         page.render({
             canvasContext: ctx,
             viewport: viewport
         });
     });
 }
+
+document.getElementById('go_previous')
+    .addEventListener('click', (e) => {
+        if (myState.pdf == null
+            || myState.currentPage == 1) return;
+        myState.currentPage -= 1;
+        document.getElementById("current_page")
+            .value = myState.currentPage;
+        render();
+    });
+
+document.getElementById('go_next')
+    .addEventListener('click', (e) => {
+        if (myState.pdf == null
+            || myState.currentPage > myState.pdf
+                ._pdfInfo.numPages)
+            return;
+
+        myState.currentPage += 1;
+        document.getElementById("current_page")
+            .value = myState.currentPage;
+        render();
+    });
+document.getElementById('current_page')
+    .addEventListener('keypress', (e) => {
+        if (myState.pdf == null) return;
+
+        // Get key code
+        var code = (e.keyCode ? e.keyCode : e.which);
+
+        // If key code matches that of the Enter key
+        if (code == 13) {
+            var desiredPage =
+                document.getElementById('current_page')
+                    .valueAsNumber;
+
+            if (desiredPage >= 1
+                && desiredPage <= myState.pdf
+                    ._pdfInfo.numPages) {
+                myState.currentPage = desiredPage;
+                document.getElementById("current_page")
+                    .value = desiredPage;
+                render();
+            }
+        }
+    });
+
+document.getElementById('current_page')
+    .addEventListener('keypress', (e) => {
+        if (myState.pdf == null) return;
+
+        // Get key code
+        var code = (e.keyCode ? e.keyCode : e.which);
+
+        // If key code matches that of the Enter key
+        if (code == 13) {
+            var desiredPage =
+                document.getElementById('current_page')
+                    .valueAsNumber;
+
+            if (desiredPage >= 1
+                && desiredPage <= myState.pdf
+                    ._pdfInfo.numPages) {
+                myState.currentPage = desiredPage;
+                document.getElementById("current_page")
+                    .value = desiredPage;
+                render();
+            }
+        }
+    });
+
+document.getElementById('zoom_in')
+    .addEventListener('click', (e) => {
+        if (myState.pdf == null) return;
+        myState.zoom += 0.5;
+        render();
+    });
+
+document.getElementById('zoom_out')
+    .addEventListener('click', (e) => {
+        if (myState.pdf == null) return;
+        myState.zoom -= 0.5;
+        render();
+    });
